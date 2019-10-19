d0ba20b7e24c462a105743650301f136365b064c
Lukasz Wierzbicki <lukasz.wierzbicki26@gmail.com>
2019-10-19 11:27:14 +0200

    init commit

---
+++ app/src/main/java/com/project/challengemine/Util/Common.kt
@@ -0,0 +1,10 @@
+package com.project.challengemine.Util
+
+import com.project.challengemine.Model.User
+
+object Common {
+    val USER_UID_SAVE_KEY: String? = "SAVE_KEY"
+    val TOKENS: String = "TOKENS"
+    lateinit var loggedUser: User
+    val USER_INFORMATION: String = "UserInformation"
+}
